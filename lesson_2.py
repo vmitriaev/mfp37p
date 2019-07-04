ccea4ec53383857ae54f58d1e57cc69c2e53035a
vmitriaev <vmitriaev@gmail.com>
2019-07-04 13:03:37 +0300

    colors was added to lesson_2.py

--- lesson_2.py
+++ lesson_2.py
@@ -1,9 +1,22 @@
 # calc v2: color
+#
+from colorama import init
+from colorama import Fore, Back, Style
+
+# use Colorama to make Termcolor work on Windows too
+init()
+
+print(Fore.BLACK)
+print(Back.GREEN)
 
 what = input("Выберите действие (+, -, *, /): ")
 
+print(Back.CYAN)
+
 a = int(input("Первое число: "))
 b = int(input("Второе число: "))
+
+print(Back.YELLOW)
 
 if what == "+":
     c = a + b
