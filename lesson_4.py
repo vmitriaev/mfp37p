fb1ab5352f339eb9fcecf2dec12972b4268f1d14
vmitriaev <vmitriaev@gmail.com>
2019-07-04 18:32:53 +0300

    first try to make api_client

--- lesson_4.py
+++ lesson_4.py
@@ -1,11 +1,55 @@
 # try to use requests library
 
 import requests
+import json
 
-r = requests.get('https://reqres.in/api/users/2')
-r.text
+baseurl = "https://reqres.in/"
+list_users = "api/users?page=2"
+single_user = "api/users/2"
 
-print(r)
-print(r.text)
-print(r.encoding)
-print(r.headers)
+choose = input("Please, choose method:\n1. list_users\n2. single_user\nmethod:")
+
+if choose == "list_users":
+
+    req = requests.get(str(baseurl) + str(list_users))
+    print(req.url)
+    print(req.status_code)
+    print(req.text)
+
+elif choose == "1":
+
+    req = requests.get(str(baseurl) + str(list_users))
+    print(req.url)
+    print(req.status_code)
+    print(req.text)
+
+elif choose == "1.":
+
+    req = requests.get(str(baseurl) + str(list_users))
+    print(req.url)
+    print(req.status_code)
+    print(req.text)
+
+elif choose == "single_user":
+
+    req = requests.get(str(baseurl) + str(single_user))
+    print(req.url)
+    print(req.status_code)
+    print(req.text)
+
+elif choose == "2":
+
+    req = requests.get(str(baseurl) + str(single_user))
+    print(req.url)
+    print(req.status_code)
+    print(req.text)
+
+elif choose == "2.":
+
+    req = requests.get(str(baseurl) + str(single_user))
+    print(req.url)
+    print(req.status_code)
+    print(req.text)
+
+else:
+    print("Didn't read LOL")
