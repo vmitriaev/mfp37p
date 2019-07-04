8002c55bf0224e2e15b44ff707ffd584ceceb9f6
vmitriaev <vmitriaev@gmail.com>
2019-07-04 12:50:45 +0300

    third comment was added to lesson_1.py

--- lesson_1.py
+++ lesson_1.py
@@ -1,3 +1,25 @@
-# firstComment
-# secondComment
-# thirdComment
+# calc v1
+
+what = input("Выберите действие (+, -, *, /): ")
+
+a = int(input("Первое число: "))
+b = int(input("Второе число: "))
+
+if what == "+":
+    c = a + b
+    print("Результат: " + str(c))
+
+elif what == "-":
+    c = a - b
+    print("Результат: " + str(c))
+
+elif what == "*":
+    c = a * b
+    print("Результат: " + str(c))
+
+elif what == "/":
+    c = a / b
+    print("Результат: " + str(c))
+
+else:
+    print("Выбрана неверная операция!")
