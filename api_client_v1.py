34ab529867db8200cc9cd4fa72df5b7f94c4c47c
vmitriaev <vmitriaev@gmail.com>
2019-07-17 17:59:59 +0300

    comment

--- api_client_v1.py
+++ api_client_v1.py
@@ -1,6 +1,56 @@
 import requests
 import json
 
-usr = requests.get('http://reqres.in/api/users/2')
 
-print(json.dumps(usr.json(), sort_keys=True, indent=4))
+def SendReq():
+    url = ('http://reqres.in/')
+
+    meth1 = ('api/users/2')
+
+    meth2 = ('api/unknown/2')
+
+    while True:
+        if send == str(1):
+            request = requests.get(url + meth1)
+            return print(json.dumps(request.json(), sort_keys=True, indent=4))
+
+        elif send == str(2):
+            request = requests.get(url + meth2)
+            return print(json.dumps(request.json(), sort_keys=True, indent=4))
+
+        else:
+            print('WAT')
+            return
+
+
+send = input('Enter method number (1/2): ')
+while True:
+    if send == str(1):
+        print(SendReq())
+        continue
+
+    elif send == str(2):
+        print(SendReq())
+        continue
+
+    elif send == ('exit'):
+        break
+
+    else:
+        print('TAW')
+
+# while True:
+# if send == str(1):
+# request = requests.get(url + meth1)
+# print(json.dumps(request.json(), sort_keys=True, indent=4))
+# continue
+
+# elif send == str(2):
+# request = requests.get(url + meth2)
+# print(json.dumps(request.json(), sort_keys=True, indent=4))
+
+# elif send == ('exit'):
+# break
+
+# else:
+# print('WAT')
