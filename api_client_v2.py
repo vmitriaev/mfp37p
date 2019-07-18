9aad0c3693cc2172e127e919d87e70f52cf3ca22
vmitriaev <vmitriaev@gmail.com>
2019-07-18 20:04:26 +0300

    comment

--- api_client_v2.py
+++ api_client_v2.py
@@ -1,6 +1,7 @@
 import requests, json
 
-methods = {0: 'http://reqres.in', 1: '/api/users/2', 2: '/api/unknown/2', 3: '/api/unknown/23', 4: '/api/users?page=2'}
+methods = {1: '/api/users/2', 2: '/api/unknown/2', 3: '/api/unknown/23', 4: '/api/users?page=2'}
+basic_url = ('http://reqres.in')
 
 
 def ReqSend():
@@ -23,15 +24,19 @@ def ReqJs():
     return ('\nResponse:' + '\n' + ReqSend() + '\n')
 
 
-while True:
-    send = input('Enter method number or stop for exit: ')
-    request_url = (methods[0] + methods[int(send)])
-    request = requests.get(request_url)
+#def GoUrl():
+#    return str(basic_url + methods[int(send)])
 
-    if int(send) in range(1, 5):
+
+while True:
+    send = str(input('Enter method number or stop for exit: '))
+
+    if send in ('1', '2', '3', '4'):
+        request_url = (basic_url + methods[int(send)])
+        request = requests.get(request_url)
         print(ReqUrl(), ReqStat(), ReqJs())
 
-    elif send == ('stop'):
+    elif send in ('stop', 'exit', '0'):
         break
 
     else:
